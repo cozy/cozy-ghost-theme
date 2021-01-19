f66e14f9b0db8df1e63d459c1c4a9d908682905e
Patrick Browne <pt.browne@gmail.com>
2021-01-19 15:27:05 +0100

    feat: Make curl fail if http status is not in 200 range

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
 set -euxo pipefail
 
 echo "Creating release..."
-curl https://api.github.com/repos/cozy/cozy-ghost-theme/releases -v -i \
+curl https://api.github.com/repos/cozy/cozy-ghost-theme/releases \
+    -f -v -i \
     -H "Content-Type:application/json" \
     -H "Authorization: token $GITHUB_TOKEN" \
     -d "{\"tag_name\":\"$TRAVIS_TAG\",\"target_commitish\": \"master\",\"name\": \"$TRAVIS_TAG\",\"body\": \"\",\"draft\": false,\"prerelease\": false}" > /tmp/release.json
@@ -13,6 +14,7 @@ echo "Release $release_id has been created"
 echo "Uploading dist/cozy.zip"
 export file="dist/cozy.zip"
 curl "https://uploads.github.com/repos/cozy/cozy-ghost-theme/releases/$release_id/assets?name=$(basename $file)" \
+    -f \
     -H "Authorization: token $GITHUB_TOKEN" \
     -H "Content-Type: $(file -b --mime-type $file)" \
     --data-binary @$file \
