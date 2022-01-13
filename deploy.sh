942a19f0dd817a6b877290a0626e912a872161bc
Sébastien Blaisot <sebastien.blaisot@cozycloud.cc>
2022-01-13 16:55:50 +0100

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,20 @@ set -euxo pipefail
 
 echo "Creating release..."
 curl https://api.github.com/repos/cozy/cozy-ghost-theme/releases \
-    -f -v -i \
     -H "Content-Type:application/json" \
     -H "Authorization: token $GITHUB_TOKEN" \
     -d "{\"tag_name\":\"$TRAVIS_TAG\",\"target_commitish\": \"master\",\"name\": \"$TRAVIS_TAG\",\"body\": \"\",\"draft\": false,\"prerelease\": false}" > /tmp/release.json
-export release_id=$(cat /tmp/release.json | jq '.[0].id')
+release_id=$(cat /tmp/release.json | jq '.[0].id')
+if [ -z "${release_id}" ]; then
+    echo "Unable to get release ID"
+    exit 1
+fi
 echo "Release $release_id has been created"
 
 echo "Uploading dist/cozy.zip"
-export file="dist/cozy.zip"
-curl "https://uploads.github.com/repos/cozy/cozy-ghost-theme/releases/$release_id/assets?name=$(basename $file)" \
+file="dist/cozy.zip"
+file_target="cozy-${TRAVIS_TAG}.zip"
+curl "https://uploads.github.com/repos/cozy/cozy-ghost-theme/releases/${release_id}/assets?name=${file_target}" \
     -f \
     -H "Authorization: token $GITHUB_TOKEN" \
     -H "Content-Type: $(file -b --mime-type $file)" \
