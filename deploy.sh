705a6274394ad1759b37cd7d4813a55027d9486d
Sébastien Blaisot <sebastien.blaisot@cozycloud.cc>
2022-01-14 00:34:32 +0100

    chore: Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,12 @@ set -euxo pipefail
 
 echo "Creating release..."
 curl https://api.github.com/repos/cozy/cozy-ghost-theme/releases \
+    -s \
     -H "Content-Type:application/json" \
     -H "Authorization: token $GITHUB_TOKEN" \
     -d "{\"tag_name\":\"$TRAVIS_TAG\",\"target_commitish\": \"master\",\"name\": \"$TRAVIS_TAG\",\"body\": \"\",\"draft\": false,\"prerelease\": false}" > /tmp/release.json
-release_id=$(cat /tmp/release.json | jq '.[0].id')
-if [ -z "${release_id}" ]; then
+release_id=$(cat /tmp/release.json | jq '.id')
+if [ -z "${release_id}" -o "${release_id}" = "null" ]; then
     echo "Unable to get release ID"
     exit 1
 fi
